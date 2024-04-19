5cdff797fbb552d911c9f324baa0e4a9af79bfcc
João Gabriel <contato.joaogabriel@proton.me>
2024-04-18 23:20:20 -0300

    New Config

--- qtile/config.py
+++ qtile/config.py
@@ -1,70 +1,97 @@
-# © https://qtile.org/ && https://www.github.com/archgabs
+from libqtile import bar, layout, qtile, widget, hook
+from libqtile.config import Click, Drag, Group, Key, Match, Screen
+from libqtile.lazy import lazy
+from libqtile.utils import guess_terminal
 
-# Qtile imports 
+from source.keybindings import returnKeys
+from source.labels import returnLabels
+from source.screenHandler import returnScreen
+from source.colors import solarized
+
 import os, subprocess
-from libqtile import hook
-
-# Personal imports
-from src.screen_vars import getElements
-from src.keybindings import modKey, launchApps, volumeControls, keyboardControls, monadtallMovements, qtileMovements, mouseEvents, changeScreen   
-from src.layouts import styleConfigs, definedLayouts, floatingConfigs
-from src.labels import groupsAndLabels
-
-### Read before editing!
-# +----------------+---------------------------------------------------------------------------------------------------------+
-# | Module         | Functionality                                                                                           |
-# +----------------+---------------------------------------------------------------------------------------------------------+
-# | screen_vars.py | Changes the Screen, Bar, thickness and background of the top panel.                                     |
-# |                | Uses the -> getElements() fn.                                                                           |
-# +----------------+---------------------------------------------------------------------------------------------------------+
-# | keybindings.py | Defines keybindings in general, use go to definition on keys = [] to be precise.                        |
-# +----------------+---------------------------------------------------------------------------------------------------------+
-# | labels.py      | Defines the labels for groups, to change keybindings of toscreen() go to definition of -> *changeScreen |
-# +----------------+---------------------------------------------------------------------------------------------------------+
 
 
-@hook.subscribe.startup
+@hook.subscribe.startup_once
 def autostart():
-    home = os.path.expanduser("~/.config/qtile/autostart.sh")
+    home = os.path.expanduser('~/.config/qtile/autostart.sh')
     subprocess.Popen([home])
 
 
-screens         = [*getElements()]
-groups          = [*groupsAndLabels]
-layouts         = [*definedLayouts]
-mouse           = [*mouseEvents]
-floating_layout = floatingConfigs
+mod = "mod4"
+terminal = "kitty"
+defaultFont = "JetBrains Mono"
 
-keys = [
-    *launchApps,
-    *volumeControls,    
-    *keyboardControls,
-    *monadtallMovements,
-    *qtileMovements,
-    *changeScreen,
-]
 
+def initGroups():
+    """ INITS THE GROUPS DEFINED IN returnLabels() """
+    for i in groups:
+        keys.extend(
+            [
+                Key(
+                    [mod],
+                    i.name,
+                    lazy.group[i.name].toscreen(),
+                    desc="Switch to group {}".format(i.name),
+                ),
+                Key(
+                    [mod, "shift"],
+                    i.name,
+                    lazy.window.togroup(i.name, switch_group=True),
+                    desc="Switch to & move focused window to group {}".format(i.name),
+                ),
+            ]
+        )
+
+
+screens = [*returnScreen()]
+keys = [*returnKeys()]
+groups = [*returnLabels()]
+layouts = [layout.MonadTall(
+    border_focus=solarized['light'],
+    border_width=6,
+    single_margin=8, margin=4
+)]
+initGroups()
 
 widget_defaults = dict(
-    font     = styleConfigs['fonts'][0],
-    fontsize = styleConfigs['styles']['fontSize'],
-    padding  = styleConfigs['styles']['padding'],
+    font=defaultFont,
+    fontsize=12,
+    padding=8,
 )
+extension_defaults = widget_defaults.copy()
 
 
+# TODO: How to modularize this?
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front()),
+]
+
+       
 dgroups_key_binder = None
 dgroups_app_rules = []  # type: list
 follow_mouse_focus = True
 bring_front_click = False
 floats_kept_above = True
 cursor_warp = False
-
+floating_layout = layout.Floating(
+    border_focus=solarized['light'],
+    border_width=6,
+    single_margin=8, margin=4,
+    float_rules=[
+        *layout.Floating.default_float_rules,
+        Match(wm_class="confirmreset"),  # gitk
+        Match(wm_class="makebranch"),  # gitk
+        Match(wm_class="maketag"),  # gitk
+        Match(wm_class="ssh-askpass"),  # ssh-askpass
+        Match(title="branchdialog"),  # gitk
+        Match(title="pinentry"),  # GPG key password entry
+    ]
+)
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
-
 auto_minimize = True
-
 wl_input_rules = None
-
 wmname = "LG3D"
